60ac5aa289a00261bc1841c0606ae9a962f3d87c
Mateus <mateushbsb@gmail.com>
2021-05-25 20:37:20 -0300

    input

--- src/components/ModalAddFood/index.tsx
+++ src/components/ModalAddFood/index.tsx
@@ -3,7 +3,7 @@ import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import { Modal } from '../Modal';
-import Input from '../Input';
+import { Input } from '../Input';
 
 interface FoodFormData {
   image: string;
@@ -30,12 +30,12 @@ export function ModalAddFood({isOpen, setIsOpen, handleAddFood}: ModalAddFoodPro
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Novo Prato</h1>
-        <Input name="image" icon="" placeholder="Cole o link aqui" />
+        <Input name="image"  placeholder="Cole o link aqui" />
 
-        <Input name="name" icon="" placeholder="Ex: Moda Italiana" />
-        <Input name="price" icon="" placeholder="Ex: 19.90" />
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
 
-        <Input name="description" icon="" placeholder="Descrição" />
+        <Input name="description" placeholder="Descrição" />
         <button type="submit" data-testid="add-food-button">
           <p className="text">Adicionar Prato</p>
           <div className="icon">
